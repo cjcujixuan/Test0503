4f5f293172094b89b0d4b4d11768f31a0e95a50e
A <110b16908@mailst.cjcu.edu.tw>
2023-05-10 14:26:49 +0800

    hi

---
+++ app/BMIAdapter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList ;
+
+public class BMIAdapter {
+    private ArrayList<BMIdata> bmIdataList;
+
+    public BMIAdapter(ArrayList<BMIdata> bmIdataList) {
+        this.bmIdataList = bmIdataList;
+    }
+
+
+}
