d14765a472cc242a71213c98cb040a60fbacecce
A <110b16908@mailst.cjcu.edu.tw>
2023-05-10 16:00:44 +0800

    hi

--- app/BMIAdapter.java
+++ app/BMIAdapter.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList ;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class BMIAdapter {
+public class BMIAdapter  {
     private ArrayList<BMIdata> bmIdataList;
 
     public BMIAdapter(ArrayList<BMIdata> bmIdataList) {
         this.bmIdataList = bmIdataList;
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    }
 
 }
